2349fad1ca6a6096500f0bbc40ecc75b3e3e4bcc
Eslim Daga <esliim9811@gmail.com>
2022-08-06 23:14:50 -0500

    Add file js

---
+++ js/index.js
@@ -0,0 +1,61 @@
+const $video = document.querySelector("#video");
+const $play = document.querySelector("#play");
+const $pause = document.querySelector("#pause");
+const $backward = document.querySelector("#backward");
+const $forward = document.querySelector("#forward");
+
+$play.addEventListener("click", handlePlay);
+$pause.addEventListener("click", handlePause);
+
+function handlePlay() {
+	$video.play();
+	$play.hidden = true;
+	$pause.hidden = false;
+}
+
+function handlePause() {
+	$video.pause();
+	$pause.hidden = true;
+	$play.hidden = false;
+}
+
+$backward.addEventListener("click", handleBackward);
+$forward.addEventListener("click", handleForward);
+
+function handleBackward() {
+	$video.currentTime -= 10;
+}
+
+function handleForward() {
+	$video.currentTime += 10;
+}
+
+const $progress = document.querySelector("#progress");
+
+//Init progress value 0
+$progress.value = 0;
+
+$video.addEventListener("loadedmetadata", handleLoaded);
+$video.addEventListener("timeupdate", handleTimeUpdate);
+$video.addEventListener("timeupdate", handleEndVideo);
+
+function handleLoaded() {
+	$progress.max = $video.duration;
+}
+
+function handleTimeUpdate() {
+	$progress.value = $video.currentTime;
+}
+
+$progress.addEventListener("input", handleInput);
+
+function handleInput() {
+	$video.currentTime = $progress.value;
+}
+
+function handleEndVideo() {
+	if ($video.currentTime === $video.duration) {
+		$pause.hidden = true;
+		$play.hidden = false;
+	}
+}
